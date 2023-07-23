7ed618c342737469ae0285a61778872c099d46ef
mayank agrawal <mayankag2060@outlook.com>
2023-07-23 19:23:07 +0530

    third commit

--- task4/ans1.js
+++ task4/ans1.js
@@ -2,6 +2,6 @@
 
 let statement = "the code will execute, if the condition becomes true";
 
-let vowels = line1.match(/[aeiou]/gi);
+let vowels = statement.match(/[aeiou]/gi);
 
 let result = console.log(vowels.length);
