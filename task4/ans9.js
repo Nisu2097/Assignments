c6fb5a265047d76b8a1e0c7f508b4fd10160bda8
mayank agrawal <mayankag2060@outlook.com>
2023-07-23 22:16:52 +0530

    third commit

---
+++ task4/ans9.js
@@ -0,0 +1,7 @@
+     // answer 1
+
+let statement = "the code will execute, if the condition becomes true";
+
+let vowels = console.log(statement.replace(/[aeiou]/gi,''));
+     
+// let result = console.log(vowels.slice());
