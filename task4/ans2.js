643b54d5257bd66ad862fb652ef30d7b124ddce3
mayank agrawal <mayankag2060@outlook.com>
2023-07-23 18:40:05 +0530

    second commit

---
+++ task4/ans2.js
@@ -0,0 +1,9 @@
+     // answer 2
+
+     let statement = "the code will execute, if the condition becomes true";
+
+     let split = line2.split("")
+     
+     let reverse = split.reverse()
+     
+     let result = console.log(reverse.join(""))
