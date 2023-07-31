2b17319dd45dc5eb8dd9c5f341dc628c3fce11b1
mayank agrawal <mayankag2060@outlook.com>
2023-07-31 14:07:24 +0530

    31/07/2023

--- task6/app.js
+++ task6/app.js
@@ -1,13 +1,29 @@
-
-
-function splitter(){
+function splitBill() {
     var amount = parseFloat(document.getElementById("amount").value);
-    var amount = parseFloat(document.getElementById("people").value);
-    var split = amount/people;
+    var people = parseInt(document.getElementById("people").value);
+    var warningElement1 = document.getElementById("warning1");
+    var warningElement2 = document.getElementById("warning2");
 
-    if(!isNaN(inputNumber)){
-        document.getElementById("result").value = split;
-    }else{
-        alert("please enter valid number");
+    // Check if the amount is a valid positive number
+    if (isNaN(amount) || amount <= 0) {
+        warningElement1.textContent = "Please enter a valid amount.";
+        return;
+    } else {
+        warningElement1.textContent = "";
     }
+
+    // Check if the number of people is greater than one
+
+    if (isNaN(people) || people <= 1) {
+        warningElement2.textContent = "No need to split the bill.";
+        return;
+    } else {
+        warningElement2.textContent = "";
+    }
+
+    // Calculate the split amount
+
+    var splitAmount = amount / people;
+    document.getElementById("result").value = `${splitAmount.toFixed(2)}`;
+
 }
