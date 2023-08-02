0373936514aaec5b8cbe37bac0c620aad188d8ae
mayank agrawal <mayankag2060@outlook.com>
2023-08-02 10:36:23 +0530

    01/08/2023

--- task7/app.js
+++ task7/app.js
@@ -2,11 +2,11 @@
 
 function taxCalculator(){
     let employeeName = document.getElementById("employeeName").value;
-    let salaryAmount = parseFloat(document.getElementById("salaryAmount").value);
+    let salary = parseFloat(document.getElementById("salaryAmount").value);
 
     //   when salary is 300000 or below 300000
 
-    if(salaryAmount<=300000){
+    if(salary<=300000){
  
      document.getElementById("deduction1").value = 0 ;
      document.getElementById("deduction2").value = 0 ;
@@ -17,8 +17,8 @@ function taxCalculator(){
 
     //   when salary is 600000 or below 600000
 
-    else if(salaryAmount <=600000){
-      let tax1 = ((salaryAmount - 300000)*0.1).toFixed(2);
+    else if(salary <=600000){
+      let tax1 = (salary - 300000)*0.1;
       document.getElementById("deduction1").value = `${tax1}`;
       document.getElementById("deduction2").value = 0;
       document.getElementById("totalTaxAmount").value = `${tax1}`;
@@ -29,16 +29,16 @@ function taxCalculator(){
 
     //   when salary is more than 600000
 
-    else if(salaryAmount>600000){
-     const  tax2 = 30000;
-     let tax3 = ((salaryAmount-600000)*0.2).toFixed(2) ;
-     let tax4 = tax2 + tax3 ; 
+    else if(salary>600000){
+     let  taxDeduction1 = 30000;
+     let taxDeduction2 = (salary-600000)*0.2;
+     let totalTaxDeduction = taxDeduction1 + taxDeduction2;
 
 
-     document.getElementById("deduction1").value = `${tax2}`
-     document.getElementById("deduction2").value = `${tax3}`
-     document.getElementById("totalTaxAmount").value = `${tax4}`
-     document.getElementById("statement").value= `Dear taxpayer ${employeeName} your salary lies in slab2 and the tax amount is 10% for upto 600000 i.e ${tax2} and 20% for above 600000 i.e ${tax3}`;
+     document.getElementById("deduction1").value = `${taxDeduction1}`
+     document.getElementById("deduction2").value = `${taxDeduction2}`
+     document.getElementById("totalTaxAmount").value = `${totalTaxDeduction}`
+     document.getElementById("statement").value= `Dear taxpayer ${employeeName} your salary lies in slab2 and the tax amount is 10% for upto 600000 i.e ${taxDeduction1} and 20% for above 600000 i.e ${taxDeduction2}`;
 
     }
 
